a484605c4f86314b21686effdf75465f00a83448
anup <anupkuppasagoudar@gmail.com>
2026-06-06 11:56:22 +0530

    additonal changes to opt in groq API key and huggingface local embeddings

--- Rag_step2.py
+++ Rag_step2.py
@@ -1,13 +1,18 @@
 import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_groq import ChatGroq
 
 load_dotenv()
-secret_key = os.getenv("Gemini_api_key")
-embeddings_model =  GoogleGenerativeAIEmbeddings(model="gemini-embedding-001", google_api_key=secret_key) 
+#secret_key = os.getenv("Gemini_api_key")
+secret_key = os.getenv("GROQ_API_KEY")
+embeddings_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+# GoogleGenerativeAIEmbeddings(model="gemini-embedding-001", google_api_key=secret_key) 
 vector_db = FAISS.load_local("My_local_vector_db", embeddings_model, allow_dangerous_deserialization=True)
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", google_api_key=secret_key)
+#llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", google_api_key=secret_key)
+llm = ChatGroq(model="llama-3.1-8b-instant", groq_api_key=secret_key, temperature=0, max_tokens=2048)
 
 
 
